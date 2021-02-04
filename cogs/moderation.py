4abe4b2d0a2770766c2547ca8b222e902b2c10fa
A.Karwowski <akk42@zips.uakron.edu>
2021-02-04 12:45:24 -0500

    fix ban permissions

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -12,49 +12,57 @@ class ModCog(commands.Cog, name='Moderation'):
         self.bot = bot
 
     @commands.command()
-    @commands.has_permissions(ban_members=True)
     # Update file when a mod bans someone
     async def ban(self, ctx, member: discord.Member = None, *, reason=""):
-        uwu = False
-        # User, Moderator, and Server
         mod = ctx.author
-        user = await self.bot.fetch_user(member.id)
-        server = ctx.guild.name
-        if reason == "" or member == None:
-            await ctx.send(f"!Ban needs a User and a Reason <@{ctx.author.id}>")
-        else:
-            # Write to file (appending)
-            async with aiosqlite.connect('/cross-bot-code/ban_list.db') as db:
-                try:
-                    await db.execute("INSERT OR IGNORE INTO ban_list(user_id, user_name, reason, date, mod, server) VALUES(?, ?, ?, ?, ?, ?)", (int(member.id), str(user), str(reason), str(date.today()), str(mod), str(server)))
-                    await db.commit()
-                except Exception as e:
+        perms = mod.guild_permissions.ban_members
+        mod_r= False
+        for r in mod_roles:
+            if r in str(mod.roles):
+                mod_r =True
+        print(mod.roles)
+        if perms or mod_r:
+            uwu = False
+            # User, Moderator, and Server
+            user = await self.bot.fetch_user(member.id)
+            server = ctx.guild.name
+            if reason == "" or member == None:
+                await ctx.send(f"!Ban needs a User and a Reason <@{ctx.author.id}>")
+            else:
+                # Write to file (appending)
+                async with aiosqlite.connect('/cross-bot-code/ban_list.db') as db:
                     try:
-                        with open('/damers-bot/banned_users.txt', mode='a') as csv_file:
-                            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                            csv_writer.writerow([member.id, user, reason, date.today(), mod, server])
-                    except Exception as EX:
-                        await ctx.send(f"Uh oh, looks like something went wrong. I fucked up.")
-                        await ctx.send(f"Send this error \n| {EX} |\n to my maintainer <@{dist}>")
-                    
-                    await ctx.send(f"Uh oh, looks like something went wrong. Dont Worry I Still recorded {user} on the CSV file for you.")
-                    await ctx.send(f"Send this error \n| {e} |\n to my maintainer <@{dist}>")
+                        await db.execute("INSERT OR IGNORE INTO ban_list(user_id, user_name, reason, date, mod, server) VALUES(?, ?, ?, ?, ?, ?)", (int(member.id), str(user), str(reason), str(date.today()), str(mod), str(server)))
+                        await db.commit()
+                    except Exception as e:
+                        try:
+                            with open('/damers-bot/banned_users.txt', mode='a') as csv_file:
+                                csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                                csv_writer.writerow([member.id, user, reason, date.today(), mod, server])
+                        except Exception as EX:
+                            await ctx.send(f"Uh oh, looks like something went wrong. I fucked up.")
+                            await ctx.send(f"Send this error \n| {EX} |\n to my maintainer <@{dist}>")
+                        
+                        await ctx.send(f"Uh oh, looks like something went wrong. Dont Worry I Still recorded {user} on the CSV file for you.")
+                        await ctx.send(f"Send this error \n| {e} |\n to my maintainer <@{dist}>")
 
-            try:
-                embed = discord.Embed(title="Banned", url="" , description="" , color=0xff0000)
-                embed.add_field(name="User", value=f"@{user}", inline=True)
-                embed.add_field(name="Reason", value=reason, inline=True)
-                embed.add_field(name="Moderator", value=f"@{mod}", inline=True)
-                embed.add_field(name="Server", value=server, inline=True)
-                embed.set_footer(text=f"<{member.id}> @{user}")
-                await ctx.send(embed=embed)
-            except Exception as e:
-                if uwu == True:
-                    await ctx.send(f"Wuh Woh Mastew. uwu. Someting Went Aww Fucky Wucky Own Me. uwu. Down't Wowwy Mastew. uwu. I was a godd wittwe bot awnd wecowded {user} fow uwu anyways. uwu")
-                    await ctx.send(f"Send this error \n| {e} |\n to my master <@{dist}>")
-                else:
-                    await ctx.send(f"Uh oh, looks like something went wrong. Dont Worry I Still recorded {user} for you.")
-                    await ctx.send(f"Send this error \n| {e} |\n to my maintainer <@{dist}>")
+                try:
+                    embed = discord.Embed(title="Banned", url="" , description="" , color=0xff0000)
+                    embed.add_field(name="User", value=f"@{user}", inline=True)
+                    embed.add_field(name="Reason", value=reason, inline=True)
+                    embed.add_field(name="Moderator", value=f"@{mod}", inline=True)
+                    embed.add_field(name="Server", value=server, inline=True)
+                    embed.set_footer(text=f"<{member.id}> @{user}")
+                    await ctx.send(embed=embed)
+                except Exception as e:
+                    if uwu == True:
+                        await ctx.send(f"Wuh Woh Mastew. uwu. Someting Went Aww Fucky Wucky Own Me. uwu. Down't Wowwy Mastew. uwu. I was a godd wittwe bot awnd wecowded {user} fow uwu anyways. uwu")
+                        await ctx.send(f"Send this error \n| {e} |\n to my master <@{dist}>")
+                    else:
+                        await ctx.send(f"Uh oh, looks like something went wrong. Dont Worry I Still recorded {user} for you.")
+                        await ctx.send(f"Send this error \n| {e} |\n to my maintainer <@{dist}>")
+        else:
+            await ctx.send(f"You don't have the right perms or roles. If you think this is an error please contact <@{dist}> about it.")
 
     @commands.command(aliases=["uplist"])
     @commands.has_permissions(administrator=True)
