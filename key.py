3392789dc40047c2e0eaf8041d1381475cd20529
Alikanakela <alexkarrrr@gmail.com>
2021-06-02 17:08:24 +0000

    Add key file

---
+++ key.py
@@ -0,0 +1,32 @@
+# Token Top Secret
+DISCORD_TOKEN='INSERT TOKEN HERE'
+
+# User list
+damers = '461277024959594508'
+dist = 109314596484939776
+suzyT = '264093084709552130'
+sid = '399623765371256833'
+
+# Actual list
+users = [damers, suzyT, sid, dist ]
+
+mod_roles = ["5713494882502574460", "571357914208403460",
+             "678726128617259038", "806931743877103617"]
+
+# Roles list
+role = '776261675065671710'
+role2= '767061737030811648'
+
+#channels to moderate with 🤡
+pcbh1 = 571373155638050839
+pcbh2 = 700039409113890856
+pcass = 742505196558483546
+perip = 715234893591871538
+booty = 648794381012172850
+clock = 742504346436108407
+sftwr = 671109813614280734
+other = 726501288694644796
+offtp = 571692235389665300
+voice = 773777846870212639
+
+discord_channel = [pcbh1,pcbh2,pcass,perip,booty,clock,sftwr,other, voice]
